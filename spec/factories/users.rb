48326d1db6f259177a52cfb647c446427e5a1e89
glee38 <glee3388@gmail.com>
2016-05-16 18:12:57 -0400

    add migrations, associations, devise to users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    confirmed_at Time.now
+    #sequence(:name) { |n| "TestUser#{n}" }
+    email "test@example.com"
+    password "please123"
   end
 end
